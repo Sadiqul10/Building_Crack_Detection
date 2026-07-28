4f40bf4090de195f115ca3fecf117902f0640df4
Sadiqul Islam <156335299+Sadiqul10@users.noreply.github.com>
2026-07-28 12:07:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ st.markdown("### About")
 st.markdown("**Intern:** Sadiqul Islam, MCA, Arunachal University of Studies")
 st.markdown("**Mentor:** Debabrat Bharali, Asst. Prof, CSE (AI & DS), Department of Engineering & Technology, USTM")
 st.markdown(
-    "Developed using **TensorFlow**, **MobileNetV2**, import streamlit as st
+    "Developed using **TensorFlow**, **MobileNetV2**, **ResNet50**, ** Grad CAM**, and **Streamlit**."
+)
